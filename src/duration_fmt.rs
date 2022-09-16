13d4a2b61b071a8299fc801a5509ffbdfb000c53
Denis Hananein <i@zloylos.me>
2022-09-16 02:02:06 +0200

    Tune durations formating

--- src/duration_fmt.rs
+++ src/duration_fmt.rs
@@ -3,7 +3,7 @@ pub fn format_duration(duration: chrono::Duration) -> String {
   let minutes = duration.num_minutes();
 
   return format!(
-    "{hours} {minutes}",
+    "{hours:>3} {minutes:>3}",
     hours = format_number(hours, "h"),
     minutes = format_number_force(minutes % 60, "m")
   );
@@ -14,30 +14,34 @@ pub fn format_duration_without_paddings(duration: chrono::Duration) -> String {
   let minutes = duration.num_minutes();
 
   return format!(
-    "{hours}{minutes}",
+    "{hours}{pad}{minutes:>3}",
     hours = format_number_without_paddings(hours, "h"),
+    pad = match hours == 0 {
+      true => "",
+      false => " ",
+    },
     minutes = format_number_force(minutes % 60, "m"),
   );
 }
 
 fn format_number(number: i64, prefix: &str) -> String {
   match number == 0 {
-    true => "".to_owned(),
+    true => String::new(),
     false => format_number_force(number, prefix),
   }
 }
 
 fn format_number_without_paddings(number: i64, prefix: &str) -> String {
-  let is_zero_and_not_minutes = number == 0 && prefix != "s";
+  let is_zero_and_not_minutes = number == 0 && prefix != "m";
   match is_zero_and_not_minutes {
-    true => "".to_owned(),
-    false => format!("{}{} ", number, prefix),
+    true => String::new(),
+    false => format!("{number}{prefix}"),
   }
 }
 
 fn format_number_force(number: i64, prefix: &str) -> String {
   match prefix {
-    "h" => format!("{:2}{}", number, prefix),
-    _ => format!("{:02}{}", number, prefix),
+    "h" => format!("{:2}{prefix}", number),
+    _ => format!("{:02}{prefix}", number),
   }
 }
